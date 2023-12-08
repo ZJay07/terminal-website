6c240913a2ca0f138d033e07baeb7b075448b0ef
Jay Choy <zcabzjc@ucl.ac.uk>
2023-12-08 15:58:30 +0000

    simple terminal

--- terminal/src/App.tsx
+++ terminal/src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [output, setOutput] = useState(['Welcome to the my Terminal! Type "help" for commands.']);
+
+  type Command = {
+    [key: string]: string;
+  };  
+
+  const commands : Command= {
+    help: 'List all available commands.',
+    // Add other commands here
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      const newOutput = [...output, `> ${inputValue}`];
+
+      if (inputValue === 'help') {
+        newOutput.push('Available commands:', ...Object.keys(commands).map(cmd => ` - ${cmd}: ${commands[cmd]}`));
+      } else if (commands[inputValue]) {
+        newOutput.push(commands[inputValue]);
+      } else {
+        newOutput.push('Unknown command.');
+      }
+
+      setOutput(newOutput);
+      setInputValue('');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="terminal">
+      {output.map((line, index) => (
+        <p key={index}>{line}</p>
+      ))}
+      <div className="input-line">
+        <span className="prompt">{'>'}</span>
+        <input 
+          name="myInput" 
+          value={inputValue} 
+          onChange={handleInputChange} 
+          onKeyDown={handleKeyDown}
+          autoFocus
+        />
+      </div>
     </div>
   );
 }
